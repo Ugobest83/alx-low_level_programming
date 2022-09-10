97790d9324dcd09d86ccc02fc66707f72fa98ff5
Ugochukwu Nneji <daloweb@yahoo.com>
2022-09-10 16:12:10 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,13 +16,14 @@
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
  * Return: 0
+ *
  */
 
 int main(void)
 {
 	int c;
 	int d;
-	int e =0;
+	int e = 0;
 
         while (e < 10)
 	{
@@ -32,7 +33,7 @@ int main(void)
 			c = 0;
 			while (c < 10)
 			{
-				if (c != d && d !=e && e < d && d < c)
+				if (c != d && d != e && e < d && d < c)
 				{
 					putchar('0' + e);
 					putchar('0' + d);
